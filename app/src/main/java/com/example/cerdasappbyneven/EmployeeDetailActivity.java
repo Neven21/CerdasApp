b6fd2a444856b99fad2e0530d6c7366f01bd8a29
Neven21 <gregoriusneven@yahoo.com>
2020-06-03 13:44:33 +0700

    edit employee FE done

--- app/src/main/java/com/example/cerdasappbyneven/EmployeeDetailActivity.java
+++ app/src/main/java/com/example/cerdasappbyneven/EmployeeDetailActivity.java
@@ -22,8 +22,11 @@ import org.w3c.dom.Text;
 public class EmployeeDetailActivity extends AppCompatActivity {
 
     String id;
+    Boolean flagdetail = true;
     TextView name, position, subject, bio, obligation, salary, classes, email, hp;
-    Button editemployeebtn;
+    EditText nameedit, positionedit, subjectedit, bioedit, salaryedit;
+    CheckBox A, B, C, D;
+    Button editemployeebtn, savechangesbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,7 @@ public class EmployeeDetailActivity extends AppCompatActivity {
         Log.d("ID in detail", id);
 
         editemployeebtn = findViewById(R.id.editemployeebtn);
+        savechangesbtn = findViewById(R.id.saveeditemployee);
         name = findViewById(R.id.name);
         position = findViewById(R.id.position);
         subject = findViewById(R.id.subject);
@@ -46,62 +50,102 @@ public class EmployeeDetailActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         hp = findViewById(R.id.hp);
 
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("employees");
+        nameedit = findViewById(R.id.nameedit);
+        positionedit = findViewById(R.id.positionedit);
+        subjectedit = findViewById(R.id.subjectedit);
+        bioedit = findViewById(R.id.bioedit);
+        salaryedit = findViewById(R.id.salaryedit);
+        A = findViewById(R.id.checkboxA);
+        B = findViewById(R.id.checkboxB);
+        C = findViewById(R.id.checkboxC);
+        D = findViewById(R.id.checkboxD);
 
-        reference.orderByChild("employeeId").equalTo(id).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot datas: dataSnapshot.getChildren()){
-                    String namedata = datas.child("name").getValue().toString();
-                    String posdata = datas.child("position").getValue().toString();
-                    String subdata = datas.child("subject").getValue().toString();
-                    String biodata = datas.child("bio").getValue().toString();
-                    String salarydata = datas.child("salary").getValue().toString();
-                    String classesdata = datas.child("classes").getValue().toString();
-                    String emaildata = datas.child("email").getValue().toString();
-                    String hpdata = datas.child("number").getValue().toString();
+            nameedit.setVisibility(View.GONE);
+            positionedit.setVisibility(View.GONE);
+            subjectedit.setVisibility(View.GONE);
+            bioedit.setVisibility(View.GONE);
+            ;
+            salaryedit.setVisibility(View.GONE);
+            A.setVisibility(View.GONE);
+            B.setVisibility(View.GONE);
+            C.setVisibility(View.GONE);
+            D.setVisibility(View.GONE);
+            savechangesbtn.setVisibility(View.GONE);
+            savechangesbtn.setClickable(false);
 
-                    name.setText(namedata);
-                    position.setText(posdata);
-                    subject.setText(subdata);
-                    bio.setText(biodata);
-                    salary.setText(salarydata);
-                    classes.setText(classesdata);
-                    email.setText(emaildata);
-                    hp.setText(hpdata);
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("employees");
 
-                    name.setVisibility(View.GONE);
+            reference.orderByChild("employeeId").equalTo(id).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot datas : dataSnapshot.getChildren()) {
+                        String namedata = datas.child("name").getValue().toString();
+                        String posdata = datas.child("position").getValue().toString();
+                        String subdata = datas.child("subject").getValue().toString();
+                        String biodata = datas.child("bio").getValue().toString();
+                        String salarydata = datas.child("salary").getValue().toString();
+                        String classesdata = datas.child("classes").getValue().toString();
+                        String emaildata = datas.child("email").getValue().toString();
+                        String hpdata = datas.child("number").getValue().toString();
 
-                    if(posdata.equals("Coordinator and Lecturer"))
-                    {
-                        obligation.setText("UMN");
-                    }
-                    else if(posdata.equals("Lecturer"))
-                    {
-                        obligation.setText("Coordinator and Lecturer");
-                    }
-                    else if(posdata.equals("Head of Lab"))
-                    {
-                        obligation.setText("UMN");
-                    }
-                    else if(posdata.equals("Lab Coordinator"))
-                    {
-                        obligation.setText("Head of Lab");
-                    }
-                    else if(posdata.equals("Lab Assistant"))
-                    {
-                        obligation.setText("Lab Coordinator");
+                        name.setText(namedata);
+                        position.setText(posdata);
+                        subject.setText(subdata);
+                        bio.setText(biodata);
+                        salary.setText(salarydata);
+                        classes.setText(classesdata);
+                        email.setText(emaildata);
+                        hp.setText(hpdata);
+
+                        nameedit.setText(namedata);
+                        positionedit.setText(posdata);
+                        subjectedit.setText(subdata);
+                        bioedit.setText(biodata);
+                        salaryedit.setText(salarydata);
+
+                        if (posdata.equals("Coordinator and Lecturer")) {
+                            obligation.setText("UMN");
+                        } else if (posdata.equals("Lecturer")) {
+                            obligation.setText("Coordinator and Lecturer");
+                        } else if (posdata.equals("Head of Lab")) {
+                            obligation.setText("UMN");
+                        } else if (posdata.equals("Lab Coordinator")) {
+                            obligation.setText("Head of Lab");
+                        } else if (posdata.equals("Lab Assistant")) {
+                            obligation.setText("Lab Coordinator");
+                        }
                     }
                 }
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+
 
         editemployeebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                nameedit.setVisibility(View.VISIBLE);
+                positionedit.setVisibility(View.VISIBLE);
+                subjectedit.setVisibility(View.VISIBLE);
+                bioedit.setVisibility(View.VISIBLE);
+                salaryedit.setVisibility(View.VISIBLE);
+                A.setVisibility(View.VISIBLE);
+                B.setVisibility(View.VISIBLE);
+                C.setVisibility(View.VISIBLE);
+                D.setVisibility(View.VISIBLE);
+                savechangesbtn.setVisibility(View.VISIBLE);
+                savechangesbtn.setClickable(true);
+
+                name.setVisibility(View.GONE);
+                position.setVisibility(View.GONE);
+                subject.setVisibility(View.GONE);
+                bio.setVisibility(View.GONE);
+                salary.setVisibility(View.GONE);
+                classes.setVisibility(View.GONE);
+                editemployeebtn.setVisibility(View.GONE);
+                editemployeebtn.setClickable(false);
 
             }
         });
